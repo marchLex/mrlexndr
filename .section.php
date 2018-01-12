f0dd3127b7dcee712eb1b0b5b0d645b05a853b2e
marchLex <a-marchenkov@yandex.ru>
2018-01-12 10:13:27 +0000

    1st

---
+++ .section.php
@@ -0,0 +1,9 @@
+<?
+$sSectionName = 'Главная';
+$arDirProperties = array(
+	'title' => 'Помни о мозгах',
+	'description' => 'Помни о мозгах',
+	'keywords' => 'мозг, mrlexndr',
+	'robots' => 'index, follow'
+);
+?>
